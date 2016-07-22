082ffc98700fb335d0897ec0636ac4b53e83c148
Linse Mathew <email": null>
2016-07-22 11:56:06 -0400

    Add parties controller.

---
+++ app/controllers/parties_controller.rb
@@ -0,0 +1,54 @@
+class PartiesController < ApplicationController
+
+  before_action :set_party, only: [:show, :update, :edit, :destroy]
+  before_action :current_user, only: [:create, :new]
+
+  def show
+    @party = Party.find_by(id: params[:id])
+  end
+
+  def new
+    @party = current_user.parties.new
+  end
+
+  def create
+    @party = current_user.parties.build(party_params)
+    if @party.save
+      flash[:success] = "There was an error saving your party"
+      redirect_to
+    else
+      @errors = @party.errors.full_messages
+      flash[:danger] = "There was an error saving your party"
+      render 'new'
+    end
+  end
+
+  def update
+    if @party.update_attributes(party_params)
+      flash[:success] = "Party updated!"
+      redirect_to @party
+    else
+      @errors = @user.errors.full_messages
+      flash.now[:danger] = "Uh oh! Something went wrong!"
+      render 'parties/edit'
+    end
+  end
+
+  def edit
+  end
+
+  def destroy
+    @party.destroy
+  end
+
+  private
+
+  def set_party
+    @party = Party.find_by(params[:id])
+  end
+
+  def party_params
+    params.require(:party).permit(:name)
+  end
+
+end
