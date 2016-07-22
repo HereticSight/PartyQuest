cd9c65f696c0d80748ec7e010b7ec10430b2caa6
Jeffrey Ng <email": null>
2016-07-22 17:45:30 -0400

    added validations to campaign, quest, user models

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -1,3 +1,6 @@
 class Quest < ActiveRecord::Base
   has_and_belongs_to_many :campaigns
+
+  validates :name, presence: true, length: { in: 6..128 }
+  validates :price_range, presence: true, length: 1
 end
