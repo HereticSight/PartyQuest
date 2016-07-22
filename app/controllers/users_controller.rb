c73f1caa56dd692cf4c7023a3daf8f6ad30bd70d
Jeffrey Ng <email": null>
2016-07-22 10:46:18 -0400

    user first name and last name now save. word formatting for error show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -67,7 +67,7 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:username, :email, :password, :bio, :city, :state, :profile_img)
+      params.require(:user).permit(:username, :email, :password, :bio, :city, :state, :profile_img, :first_name, :last_name)
     end
 
 end
