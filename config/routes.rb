b81d0bd4767a0ca96af377ec61f58f1b90762b6e
Kenny Phu <kphu@outlook.com>
2016-07-22 14:53:22 -0400

    Create index page
    Created an overall home page and static pages controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  # root 'StaticPagesController#index'
+  root 'static_pages#index'
 
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  
+
   resources :users, path_names: {new: 'signup', create: 'signup'}
 end
