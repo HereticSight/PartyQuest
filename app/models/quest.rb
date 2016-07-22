8d8114ddfe8fce633868e94e0dd1fde4675665b0
Jeffrey Ng <email": null>
2016-07-22 18:23:19 -0400

    updated user show page, updated validation for quests

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -2,5 +2,5 @@ class Quest < ActiveRecord::Base
   has_and_belongs_to_many :campaigns
 
   validates :name, presence: true, length: { in: 6..128 }
-  validates :price_range, presence: true, length: 1, inclusion: { in: [1, 2, 3, 4, 5] }
+  validates :price_range, presence: true, inclusion: { in: [1, 2, 3, 4, 5] }
 end
