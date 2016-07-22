742cd299d383ae344ce73173f7574c4d50f5b504
Jeffrey Ng <email": null>
2016-07-22 14:13:49 -0400

    ALL ASSOCIATIONS WORK

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -1,5 +1,5 @@
 class Quest < ActiveRecord::Base
-  has_one :location
+  belongs_to :location
   has_and_belongs_to_many :activities
   belongs_to :party
   has_one :creator, through: :party, source: :leader
