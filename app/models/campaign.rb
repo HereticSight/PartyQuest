df875a1b427b058561b65dcb5edcc3007afe1192
Jeffrey Ng <jeffreymng@netzero.net>
2016-07-23 11:56:04 -0400

    added custom validation for end date after start date

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -8,10 +8,17 @@ class Campaign < ActiveRecord::Base
   validates :leader_id, presence: true
 
   validate :start_date_in_future
+  validate :end_date_after_start_date
 
   def start_date_in_future
     if start_time.present? && start_time < DateTime.now
       errors.add(:start_time, "Don't live in the past! Campaigns can only be started in the future!")
+    end
+  end
+
+  def end_date_after_start_date
+    if end_time.present? && end_time < start_time
+      errors.add(:end_time, "Campaigns must end after your start date!")
     end
   end
 
