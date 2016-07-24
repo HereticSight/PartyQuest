b86d3fd2a7b038a6d3b03f9310ee95de14529781
Jeffrey Ng <jeffreymng@netzero.net>
2016-07-24 12:24:57 -0400

    routes now make more sense. simplified code in locations controller by using provided helper methods

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,41 +1,51 @@
 class LocationsController < ApplicationController
 
-   before_action :current_user, only: [:create, :new, :edit, :destroy]
-
   def index
     @locations = Location.all
   end
 
   def show
-    @campaign = Campaign.find_by(id: params[:campaign_id])
-    @location = @campaign.location
+    @location = Location.find_by(id: params[:id])
   end
 
   def new
-    if @current_user
-      @campaign = Campaign.find_by(id: params[:campaign_id])
-      if @current_user.id == @campaign.leader_id
-        @location = @campaign.build_location
-      else
-        redirect_to @campaign
-      end
+    login_redirect 
+    @campaign = Campaign.find_by(id: params[:campaign_id])
+    if current_user?(@campaign.leader)
+      @location = @campaign.build_location
     else
-      redirect_to '/login'
+      redirect_to @campaign
     end
+    # ^^^ used the helper methods to simplify code
+    # if @current_user
+    #   @campaign = Campaign.find_by(id: params[:campaign_id])
+    #   if @current_user.id == @campaign.leader_id
+    #     @location = @campaign.build_location
+    #   else
+    #     redirect_to @campaign
+    #   end
+    # else
+    #   redirect_to '/login'
+    # end
   end
 
   def create
     @campaign = Campaign.find_by(id: params[:campaign_id])
     @location = Location.find_or_initialize_by(location_params)
-    if @location.save
-      flash[:success] = "You've successfully created your location!"
-      @campaign.location = @location
-      @campaign.save
-      redirect_to new_campaign_quest_path(@campaign)
+    login_redirect
+    if current_user?(@campaign.leader)
+      if @location.save
+        flash[:success] = "You've successfully created a location!"
+        @campaign.location = @location
+        @campaign.save
+        redirect_to new_campaign_quest_path(@campaign)
+      else
+        @errors = @location.errors.full_messages
+        flash[:danger] = "Oops! We couldn't create your location!"
+        render 'new'
+      end
     else
-      @errors = @location.errors.full_messages
-      flash[:danger] = "Oops! We couldn't create your location!"
-      render 'new'
+      redirect_to @campaign
     end
   end
 
