d3af83cba4506ec48a134ca5276e66a2a104da47
Linse Mathew <email": null>
2016-07-25 09:43:49 -0400

    Add search logic in search show page.

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,5 @@
 class Location < ActiveRecord::Base
-  has_many :campaigns
-
   include PgSearch
   multisearchable :against => [:latitude, :longitude, :address, :city, :state]
-
+  has_many :campaigns
 end
