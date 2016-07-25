db7bfe71ea9d9978a9bebb8892df2916e8e66db5
Kenny Phu <kphu@outlook.com>
2016-07-25 10:20:10 -0400

    Create preliminary objects to contain maps. Add recent campaigns to index page

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,23 +1,20 @@
 "use strict"
 
-var markersArray = []
-
 function initMap() {
-  if ($( '#map' )) {
+  if ($( '#map' ).length > 0) {
+    var markersArray = []
     var geocoder = new google.maps.Geocoder();
     var map = new google.maps.Map(document.getElementById( 'map' ), {
       center: {lat: 40.72902144999053, lng: -73.99128341814503},
       zoom: 12,
-      clickableIcons: false
+      clickableIcons: false,
+      mapTypeControl: false,
+      zoomControl: false,
+      streetViewControl: false,
     });
     map.addListener('click', function(e) {
       placeMarkerAndPanTo(e.latLng, map);
       updateFields(e.latLng);
-      // geocoder.geocode({location: e.latLng}, function(results, status) {
-      //   if (status === google.maps.GeocoderStatus.OK) {
-      //     $( '#location_raw_address' ).val(results[0].formatted_address)
-      //   };
-      // });
     });
     $( '#find-on-map' ).on('click', function(e) {
       e.preventDefault();
@@ -27,11 +24,14 @@ function initMap() {
         };
       });
     });
-  } else if ($( '#index_map' )) {
+  } else if ($( '#index_map' ).length > 0) {
       var index_map = new google.maps.Map(document.getElementById( 'index_map' ), {
         center: {lat: 40.72902144999053, lng: -73.99128341814503},
         zoom: 12,
-        clickableIcons: false
+        clickableIcons: false,
+        mapTypeControl: false,
+        zoomControl: false,
+        streetViewControl: false,
     });
   };
 };
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def index
+    @campaigns = Campaign.order(start_time: :desc).limit(5)
     render 'static_pages/index'
   end
 end
