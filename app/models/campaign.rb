13658cebc59c36e3cd60b5f36fccc5591d9101a6
Linse Mathew <email": null>
2016-07-25 18:06:01 -0400

    Add scrolling pics on campaigns.

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,7 +1,7 @@
 class Campaign < ActiveRecord::Base
   include PgSearch
   multisearchable :against => [:name], if: :active_campaign?
-  has_many :pictures
+  has_many :pictures, :dependent => :destroy
   belongs_to :leader, class_name: "User", foreign_key: :leader_id
   has_and_belongs_to_many :users
   has_and_belongs_to_many :quests
