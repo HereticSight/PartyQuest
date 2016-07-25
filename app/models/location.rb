6e1a5ccf6f8d0949cd66ab8324a908e31723758b
Linse Mathew <email": null>
2016-07-25 09:08:54 -0400

    Change search algorithm.

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,12 +1,7 @@
 class Location < ActiveRecord::Base
   has_many :campaigns
+
   include PgSearch
-  pg_search_scope :search_full_text, :against => {
-    :latitude => 'A',
-    :longitude => 'A',
-    :address => 'B',
-    :city => 'C',
-    :state => 'D'
-  }
+  multisearchable :against => [:latitude, :longitude, :address, :city, :state]
 
 end
